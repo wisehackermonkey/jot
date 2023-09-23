bda4aa95880979f6dacfebb88281725b28bc61b2
Oran C <oranbusiness@gmail.com>
2023-09-22 21:44:54 -0700

    working resizable

--- src/main.c
+++ src/main.c
@@ -1,120 +1,53 @@
 #include <windows.h>
 
-// Global variables
-HWND g_hWnd;
-HWND g_hEdit;
-COLORREF g_crBackground = RGB(68, 70, 84); 
+#define ID_TEXTBOX 1
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
 
-  switch(msg) {
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow) {
+    WNDCLASSW wc = {0};
 
-    case WM_CTLCOLOREDIT:
-    {
-      HDC hdcEdit = (HDC) wParam;
-      SetTextColor(hdcEdit, RGB(255, 255, 255));
-      SetBkColor(hdcEdit, g_crBackground); 
-      return (INT_PTR)CreateSolidBrush(g_crBackground);
+    wc.hbrBackground = (HBRUSH)CreateSolidBrush(RGB(0x44, 0x46, 0x54));
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hInstance = hInst;
+    wc.lpszClassName = L"TextEditor";
+    wc.lpfnWndProc = WindowProcedure;
+
+    if(!RegisterClassW(&wc))
+        return -1;
+
+    CreateWindowW(L"TextEditor", L"Simple Text Editor", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 600, NULL, NULL, NULL, NULL);
+
+    MSG msg = {0};
+
+    while(GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
 
-  }
-
-  return DefWindowProc(hWnd, msg, wParam, lParam);
+    return 0;
 }
 
-// Function to create the main window
-BOOL CreateMainWindow(HINSTANCE hInstance, int nCmdShow) {
+LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+    static HWND hwndTextbox;
 
-  // Register window class
-  WNDCLASSEX wc;
-  wc.cbSize = sizeof(WNDCLASSEX);
-  wc.style = CS_HREDRAW | CS_VREDRAW; 
-  wc.lpfnWndProc = WndProc;
-  wc.cbClsExtra = 0;
-  wc.cbWndExtra = 0;
-  wc.hInstance = hInstance;
-  wc.hIcon = NULL;
-  wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-  wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1); 
-  wc.lpszMenuName = NULL;
-  wc.lpszClassName = TEXT("MyWindowClass");
-  wc.hIconSm = NULL;
-  RegisterClassEx(&wc);
+    switch(msg) {
+        case WM_CREATE:
+            hwndTextbox = CreateWindowW(L"Edit", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | WS_VSCROLL | ES_MULTILINE | ES_AUTOVSCROLL, 
+                                        0, 0, 500, 600, hwnd, (HMENU)ID_TEXTBOX, NULL, NULL);
+            break;
+        
+        case WM_SIZE:
+            MoveWindow(hwndTextbox, 0, 0, LOWORD(lp), HIWORD(lp), TRUE);
+            break;
+        
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        
+        default:
+            return DefWindowProcW(hwnd, msg, wp, lp);
+    }
 
-  // Create main window
-  g_hWnd = CreateWindow(
-      TEXT("MyWindowClass"),
-      TEXT("My Window"),
-      WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, CW_USEDEFAULT,
-      500, 600,
-      NULL, NULL, hInstance, NULL);
-
-  if (!g_hWnd) {
-    return FALSE;
-  }
-
-  // Set main window background color
-  SetClassLongPtr(g_hWnd, GCLP_HBRBACKGROUND, 
-    (LONG_PTR)CreateSolidBrush(RGB(255, 250, 0)));
-
-  // Create font
-  HFONT hFont = CreateFont(14, 0, 0, 0, FW_NORMAL, FALSE, FALSE, FALSE, 
-    ANSI_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, 
-    DEFAULT_QUALITY, FF_DONTCARE|FIXED_PITCH, TEXT("Consolas"));
-
-  // Set main window font
-  SendMessage(g_hWnd, WM_SETFONT, (WPARAM)hFont, TRUE);
-
-  // Register edit control class
-  WNDCLASSEX wcEdit;
-  wcEdit.cbSize = sizeof(WNDCLASSEX);
-  wcEdit.style = CS_HREDRAW | CS_VREDRAW;
-  wcEdit.lpfnWndProc = DefWindowProc;
-  wcEdit.cbClsExtra = 0;
-  wcEdit.cbWndExtra = 0;
-  wcEdit.hInstance = hInstance;
-  wcEdit.hIcon = NULL;
-  wcEdit.hCursor = LoadCursor(NULL, IDC_IBEAM);
-  wcEdit.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-  wcEdit.lpszMenuName = NULL;
-  wcEdit.lpszClassName = TEXT("EditControl");
-  wcEdit.hIconSm = NULL;
-  RegisterClassEx(&wcEdit);
-
-
-// Create edit control
-g_hEdit = CreateWindowEx(
-    WS_EX_CLIENTEDGE,
-    TEXT("EDIT"), 
-    NULL,
-    WS_CHILD | WS_VISIBLE | ES_AUTOHSCROLL | ES_AUTOVSCROLL,
-    0, 0, 500, 500,
-    g_hWnd,
-    (HMENU)1, 
-    hInstance, 
-    NULL);
-
-  ShowWindow(g_hWnd, nCmdShow);
-  UpdateWindow(g_hWnd);
-
-  return TRUE;
-}
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, 
-  LPSTR lpCmdLine, int nCmdShow) {
-  
-  UNREFERENCED_PARAMETER(hPrevInstance);
-  UNREFERENCED_PARAMETER(lpCmdLine);
-
-  if(!CreateMainWindow(hInstance, nCmdShow))
-    return 1;
-
-  MSG msg;
-  while(GetMessage(&msg, NULL, 0, 0)) {
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
-  }
-
-  return (int)msg.wParam;
+    return 0;
 }
